620dc04d60dfc02aa02bd0a174f0a96f8c98e93f
Egor Petrov <egor@barbatosso.com>
2019-11-16 13:48:03 +0200

    Init commit

---
+++ ARAngryBirds/ARAngryBirds/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ARAngryBirds
+//
+//  Created by  Egor Petrov on 11/16/19.
+//  Copyright © 2019  Egor Petrov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
